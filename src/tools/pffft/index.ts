a12e29e8809d089ed9f6768a34b38b5ea7b403e2
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-12-31 17:26:35 +0530

    pfft

--- src/tools/pffft/index.ts
+++ src/tools/pffft/index.ts
@@ -1,6 +1,14 @@
-import pffft_wasm from './pffft.wasm?init';
+let wasm: WebAssembly.WebAssemblyInstantiatedSource = null!;
 
 export const get_pffft_instance = async () => {
-  const instance = await pffft_wasm();
-  return instance.exports;
+  const url = new URL('/src/tools/pffft/pffft.wasm', import.meta.url);
+
+  // write te above using async/await
+  if (!wasm) {
+    console.log('Loading wasm');
+    const wasm_obj = await fetch(url).then((x) => x.arrayBuffer());
+    wasm = await WebAssembly.instantiate(wasm_obj);
+    console.log('Loaded wasm', wasm);
+  }
+  return wasm;
 };
